61f0c0d42b1529243adfc622a790c0108ddbc5a4
ask0later <139331444+ask0later@users.noreply.github.com>
2024-02-10 13:23:10 +0300

    Create codes.asm

---
+++ codes.asm
@@ -0,0 +1,41 @@
+UpLFrame equ 0d5h
+UpMFrame equ 0d1h
+UpRFrame equ 0b8h
+
+VertFrame equ 0b3h
+HorFrame  equ 0cdh
+
+LowLFrame equ 0d4h
+LowMFrame equ 0cfh
+LowRFrame equ 0beh
+
+VideoMemAddress equ 0b800h
+
+
+TitleLength equ 8d
+FrameLength equ 30d
+FrameHeight equ 10d
+
+LineLength equ 80d
+ByteWord equ 2d
+
+HeartCode equ 3h
+k_code equ 06bh
+a_code equ 061h
+r_code equ 072h
+i_code equ 069h
+n_code equ 06eh
+
+
+WhiteOnGrey equ 7Fh
+GreyOnGrey equ 77h
+GreyOnMagenta equ 57h
+WhiteOnMagenta equ 5fh
+MagentaOnRed equ 4dh
+WhiteOnRed equ 4fh
+
+
+
+
+
+
